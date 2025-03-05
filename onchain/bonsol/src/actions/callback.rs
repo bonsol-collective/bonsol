f72b02c8d7afaf6625f6353648ff7878504eba5b
githubsands <ryvace@protonmail.com>
2025-03-05 17:21:18 +0000

    wip

---
+++ onchain/bonsol/src/actions/callback.rs
@@ -0,0 +1,34 @@
+use crate::{
+    assertions::*,
+    error::ChannelError,
+    proof_handling::{
+        output_digest_v1_0_1, output_digest_v1_2_1, prepare_inputs_v1_0_1, prepare_inputs_v1_2_1,
+        verify_risc0_v1_0_1, verify_risc0_v1_2_1,
+    },
+    utilities::*,
+};
+
+use bonsol_interface::{
+    bonsol_schema::{
+        root_as_execution_request_v1, ChannelInstruction, ExecutionRequestV1, ExitCode, StatusV1,
+    },
+    prover_version::{ProverVersion, VERSION_V1_0_1, VERSION_V1_2_1},
+    util::execution_address_seeds,
+};
+
+use solana_program::{
+    account_info::AccountInfo,
+    clock::Clock,
+    instruction::{AccountMeta, Instruction},
+    msg,
+    program::invoke_signed,
+    program_error::ProgramError,
+    program_memory::sol_memcmp,
+    sysvar::Sysvar,
+};
+
+struct CallbackAccount<'a> {
+    requester: &'a AccountInfo<'a>, // requester is prover
+    exec_account: &'a AccountInfo<'a>,
+    callback_accounts: &'a AccountInfo<'a>,
+}
--- onchain/bonsol/src/actions/mod.rs
+++ onchain/bonsol/src/actions/mod.rs
@@ -1,11 +1,12 @@
+mod callback;
 mod claim;
 mod deploy;
 mod execute;
 mod proof;
 mod status;
 
+// pub use callback::*;
 pub use claim::*;
 pub use deploy::*;
 pub use execute::*;
-pub use proof::*;
 pub use status::*;
