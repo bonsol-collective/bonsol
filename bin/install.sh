cfdd6b8efb7406f6f122437a1b79ac62722b5fa4
Augustin Chan <aug@iterative.day>
2025-03-10 19:17:21 +0800

    feat: add --force flag to cargo install commands to ensure latest version

--- bin/install.sh
+++ bin/install.sh
@@ -53,11 +53,11 @@ Linux)
     # check if nvidia-smi exists and nvcc is available
     if command -v nvidia-smi >/dev/null 2>&1 && command -v nvcc >/dev/null 2>&1; then
         echo "installing with cuda support"
-        RUSTFLAGS="-C target-cpu=native" cargo install bonsol-cli --git https://github.com/bonsol-collective/bonsol --features linux --locked
+        RUSTFLAGS="-C target-cpu=native" cargo install bonsol-cli --git https://github.com/bonsol-collective/bonsol --features linux --locked --force
     else
         echo "installing without cuda support, proving will be slower"
         # Install without any specific features for non-CUDA systems
-        RUSTFLAGS="-C target-cpu=native" cargo install bonsol-cli --git https://github.com/bonsol-collective/bonsol --locked
+        RUSTFLAGS="-C target-cpu=native" cargo install bonsol-cli --git https://github.com/bonsol-collective/bonsol --locked --force
     fi
     ;;
 Darwin)
@@ -67,7 +67,7 @@ Darwin)
         brew install flatbuffers
         echo "flatc installation complete"
     fi
-    cargo install bonsol-cli --git https://github.com/bonsol-collective/bonsol --features mac --locked
+    cargo install bonsol-cli --git https://github.com/bonsol-collective/bonsol --features mac --locked --force
     ;;
 *)
     echo "Unsupported operating system: $OS"
