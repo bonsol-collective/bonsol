ccb2efe1113d80cd029459264a6409944b7d4a30
Augustin Chan <aug@iterative.day>
2025-03-10 18:19:11 +0800

    fix: explicitly disable cuda features when cuda tools are not available

--- bin/install.sh
+++ bin/install.sh
@@ -9,13 +9,14 @@ rzup install cargo-risczero 1.2.1
 OS=$(uname -s)
 case "$OS" in
 Linux)
-    # check if nvidia-smi exists
-    if ! command -v nvidia-smi >/dev/null 2>&1; then
-        echo "installing without cuda support, proving will be slower"
-        cargo install bonsol-cli --git https://github.com/bonsol-collective/bonsol
-    else
+    # check if nvidia-smi exists and nvcc is available
+    if command -v nvidia-smi >/dev/null 2>&1 && command -v nvcc >/dev/null 2>&1; then
         echo "installing with cuda support"
         cargo install bonsol-cli --git https://github.com/bonsol-collective/bonsol --features linux
+    else
+        echo "installing without cuda support, proving will be slower"
+        # Explicitly disable cuda features
+        RUSTFLAGS='' cargo install bonsol-cli --git https://github.com/bonsol-collective/bonsol --no-default-features
     fi
     ;;
 Darwin)
