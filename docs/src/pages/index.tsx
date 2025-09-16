3a668c0872613b666d77973bb36f2b5c1cc10598
Daniel Idowu <74189249+dharniel45@users.noreply.github.com>
2025-09-16 15:56:16 +0100

    changes to main page

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -19,8 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/tutorials/a-taste-of-bonsol">
-            Bonsol Tutorial - 1hr ⏱️
+            to="https://docs.bonsol.org/">
+            View the updated version here
           </Link>
         </div>
       </div>
